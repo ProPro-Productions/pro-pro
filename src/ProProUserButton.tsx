196dbf27c7fa4a4f34e43b4295a1a92f0dd53c80
Chidozie C. Okafor <39489124+doziestar@users.noreply.github.com>
2023-11-16 13:14:35 +0100

    Add user menu popup to ProProUserButton component

--- src/ProProUserButton.tsx
+++ src/ProProUserButton.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useAuth } from "./AuthContext";
 
 interface ProProUserButtonProps {
@@ -9,16 +9,56 @@ const ProProUserButton: React.FC<ProProUserButtonProps> = ({
   afterSignOutUrl = "/",
 }) => {
   const { user, signOut } = useAuth();
+  const [showMenu, setShowMenu] = useState(false);
 
   const handleSignOut = () => {
     signOut();
     window.location.href = afterSignOutUrl;
   };
 
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
   return (
-    <button onClick={handleSignOut} className="sign-out-button">
-      Sign Out {user?.name}
-    </button>
+    <div className="user-button-container">
+      <button
+        type="button"
+        className="user-avatar-button"
+        onClick={toggleMenu}
+        onKeyDown={(event) => {
+          if (event.key === "Enter" || event.key === " ") {
+            toggleMenu();
+          }
+        }}
+      >
+        <img
+          src={user?.avatar ?? "default-avatar.png"}
+          alt="User Avatar"
+          className="user-avatar"
+        />
+      </button>
+      {showMenu && (
+        <div className="user-menu-popup">
+          <span className="username">{user?.username}</span>
+          <span className="name">{user?.name}</span>
+          <ul>
+            <li>
+              <button
+                onClick={() => {
+                  window.location.href = "/account";
+                }}
+              >
+                Manage account
+              </button>
+            </li>
+            <li>
+              <button onClick={handleSignOut}>Sign out</button>
+            </li>
+          </ul>
+        </div>
+      )}
+    </div>
   );
 };
 
