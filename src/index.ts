c420feb0c6ea93bb2af355c67ccdbd3a218454c2
Chidozie C. Okafor <39489124+doziestar@users.noreply.github.com>
2023-11-16 13:19:37 +0100

    Add Tailwind CSS and import global styles

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import "./global.css";
 export { default as ProProUserButton } from "./ProProUserButton";
 export { AuthProvider, useAuth, withAuth } from "./AuthContext";
 export { default as proproAuth } from "./ProProAuth";
